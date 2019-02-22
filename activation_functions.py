e0b7f42ea984de465e18de992fcb45ce2c88a751
Daniel Aragão Abreu Filho <danielfilhoce@gmail.com>
2019-02-22 02:22:31 -0300

    funções de ativação

--- activation_functions.py
+++ activation_functions.py
@@ -1,2 +1,61 @@
-def exp(x):
-    return x**2
+import math
+
+class PartiallyDiff:
+
+    @staticmethod
+    def hard_limiter(u):
+        return 0 if u < 0 else 1
+
+    @staticmethod
+    def symmetric_hard_limiter(u):
+        return 0 if u == 0 else (1 if u > 0 else -1)
+
+    @staticmethod
+    def symmetrical_ramp(u, a):
+        return a if u > a else (-a if u < -a else u)
+
+    @staticmethod
+    def rectified_linear_unit(u):
+        return u if u > 0 else 0
+
+    @staticmethod
+    def parametric_rectified_linear_unit(u, alpha):
+        if alpha <= 0:
+            raise Exception("alpha > 0")
+        
+        return u if u > 0 else alpha*u
+
+class FullyDiff:
+
+    @staticmethod
+    def logistic(u, beta):
+        if beta <= 0:
+            raise Exception("beta > 0")
+
+        return 1/(1 + math.e ** (-beta * u))
+
+    @staticmethod
+    def Hyperbolic_Tangent(u, beta):
+        if beta <= 0:
+            raise Exception("beta > 0")
+        
+        return (1 - math.e ** (-beta * u))/(1 + math.e ** (-beta * u))
+    
+    @staticmethod
+    def gaussian(u, c, sigma):
+        if sigma != 0:
+            raise Exception("sigma > 0")
+            
+        return math.e ** -(((u - c) ** 2)/(2 * (sigma ** 2)))
+    
+    @staticmethod
+    def linear(u):
+        return u
+    
+    @staticmethod
+    def exponential_linear_unit(u, alpha):
+        if alpha <= 0:
+            raise Exception("alpha > 0")
+        
+        return u if u > 0 else alpha * ((math.e ** u) - 1)
+
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from neuron import Neuron
-from activation_functions import exp
+from activation_functions import PartiallyDiff
 
-neuron = Neuron()
-neuron.activation_function = exp
+neuron = Neuron(2, [1, 1], [1, 1], PartiallyDiff.hard_limiter)
 
-print(neuron.activation_function(2))
+print(neuron)
+print("Result: " + str(neuron.output()))
 
+neuron.inputs = [1,0]
+print(neuron)
+print("Result: " + str(neuron.output()))
 
+neuron.inputs = [0,1]
+print(neuron)
+print("Result: " + str(neuron.output()))
+
+neuron.inputs = [0,0]
+print(neuron)
+print("Result: " + str(neuron.output()))
