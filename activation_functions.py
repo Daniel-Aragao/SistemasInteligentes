3abe866fbd26baaca89121e169488e927a6053bd
Daniel Aragão Abreu Filho <danielfilhoce@gmail.com>
2019-02-25 03:24:54 -0300

    10 Funções  dos slides

--- activation_functions.py
+++ activation_functions.py
@@ -3,59 +3,105 @@ import math
 class PartiallyDiff:
 
     @staticmethod
-    def hard_limiter(u):
+    def step(u):
         return 0 if u < 0 else 1
+    
+    @staticmethod
+    def step_derivative(u):
+        return 0
 
     @staticmethod
-    def symmetric_hard_limiter(u):
+    def signal(u):
         return 0 if u == 0 else (1 if u > 0 else -1)
 
     @staticmethod
-    def symmetrical_ramp(u, a):
+    def signal_derivative(u):
+        return 0
+
+    @staticmethod
+    def symmetrical_ramp(u, a=2):
         return a if u > a else (-a if u < -a else u)
+
+    @staticmethod
+    def symmetrical_ramp_derivative(u, a=2):
+        return 0 if u > a else (0 if u < -a else 1)
 
     @staticmethod
     def rectified_linear_unit(u):
         return u if u > 0 else 0
 
     @staticmethod
-    def parametric_rectified_linear_unit(u, alpha):
+    def rectified_linear_unit_derivative(u):
+        return PartiallyDiff.step(u)
+
+    @staticmethod
+    def parametric_rectified_linear_unit(u, alpha=0.2):
         if alpha <= 0:
             raise Exception("alpha > 0")
         
         return u if u > 0 else alpha*u
+    
+    @staticmethod
+    def parametric_rectified_linear_unit_derivative(u, alpha=0.2):
+        if alpha <= 0:
+            raise Exception("alpha > 0")
+        
+        return 1 if u > 0 else alpha
 
 class FullyDiff:
 
     @staticmethod
-    def logistic(u, beta):
+    def logistic(u, beta=0.5):
         if beta <= 0:
             raise Exception("beta > 0")
 
         return 1/(1 + math.e ** (-beta * u))
+    
+    @staticmethod
+    def logistic_derivative(u):
+        return FullyDiff.logistic(u) * (1 - FullyDiff.logistic(u))
 
     @staticmethod
-    def Hyperbolic_Tangent(u, beta):
+    def hyperbolic_tangent(u, beta=0.4):
         if beta <= 0:
             raise Exception("beta > 0")
         
         return (1 - math.e ** (-beta * u))/(1 + math.e ** (-beta * u))
     
     @staticmethod
-    def gaussian(u, c, sigma):
-        if sigma != 0:
-            raise Exception("sigma > 0")
+    def hyperbolic_tangent_derivative(u):
+        return 1 - (FullyDiff.hyperbolic_tangent(u))**2
+    
+    @staticmethod
+    def gaussian(u, c=1, sigma=2):
+        if sigma == 0:
+            raise Exception("sigma == 0")
             
         return math.e ** -(((u - c) ** 2)/(2 * (sigma ** 2)))
     
     @staticmethod
-    def linear(u):
-        return u
+    def gaussian_derivative(u, c=1, sigma=2):
+        return -(((math.e ** (-(((u - c) ** 2)/(2 * (sigma ** 2))))) * u - c)/(sigma ** 2))
     
     @staticmethod
-    def exponential_linear_unit(u, alpha):
+    def linear(u):
+        return u
+
+    @staticmethod
+    def linear_derivative(u):
+        return 1
+    
+    @staticmethod
+    def exponential_linear_unit(u, alpha=1):
         if alpha <= 0:
             raise Exception("alpha > 0")
         
         return u if u > 0 else alpha * ((math.e ** u) - 1)
+    
+    staticmethod
+    def exponential_linear_unit_derivative(u, alpha=1):
+        return 1 if u > 0 else FullyDiff.exponential_linear_unit(u, alpha) + alpha
+    
+
+    
 
---
+++ plot_functions.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+# import numpy as np
+
+import activation_functions as af
+
+
+data = [x * 0.1 for x in range(-200, 201)]
+
+# data_step = list(map(af.PartiallyDiff.step, data))
+# data_step_derivative = list(map(af.PartiallyDiff.step_derivative, data))
+# plt.plot(data, data_step, label='Degrau')
+# plt.plot(data, data_step_derivative, label='Derivada Degrau')
+
+# data_signal = list(map(af.PartiallyDiff.signal, data))
+# data_signal_derivative = list(map(af.PartiallyDiff.signal_derivative, data))
+# plt.plot(data, data_signal, label='Degrau bipolar')
+# plt.plot(data, data_signal_derivative, label='Derivada Degrau bipolar')
+
+# data_symmetrical_ramp = list(map(af.PartiallyDiff.symmetrical_ramp, data))
+# data_symmetrical_ramp_derivative = list(map(af.PartiallyDiff.symmetrical_ramp_derivative, data))
+# plt.plot(data, data_symmetrical_ramp, label='Rampa simétrica')
+# plt.plot(data, data_symmetrical_ramp_derivative, label='Derivada Rampa simétrica')
+
+# data_rectified_linear_unit = list(map(af.PartiallyDiff.rectified_linear_unit, data))
+# data_rectified_linear_unit_derivative = list(map(af.PartiallyDiff.rectified_linear_unit_derivative, data))
+# plt.plot(data, data_rectified_linear_unit, label='RELU')
+# plt.plot(data, data_rectified_linear_unit_derivative, label='Derivada RELU')
+
+# data_parametric_rectified_linear_unit = list(map(af.PartiallyDiff.parametric_rectified_linear_unit, data))
+# data_parametric_rectified_linear_unit_derivative = list(map(af.PartiallyDiff.parametric_rectified_linear_unit_derivative, data))
+# plt.plot(data, data_parametric_rectified_linear_unit, label='PRELU')
+# plt.plot(data, data_parametric_rectified_linear_unit_derivative, label='Derivada PRELU')
+
+# data_logistic = list(map(af.FullyDiff.logistic, data))
+# data_logistic_derivative = list(map(af.FullyDiff.logistic_derivative, data))
+# plt.plot(data, data_logistic, label='Logísitca')
+# plt.plot(data, data_logistic_derivative, label='Derivada Logística')
+
+# data_hyperbolic_tangent = list(map(af.FullyDiff.hyperbolic_tangent, data))
+# data_hyperbolic_tangent_derivative = list(map(af.FullyDiff.hyperbolic_tangent_derivative, data))
+# plt.plot(data, data_hyperbolic_tangent, label='Tangente Hiperbólica')
+# plt.plot(data, data_hyperbolic_tangent_derivative, label='Derivada Tangente Hiperbólica')
+
+# data_gaussian = list(map(af.FullyDiff.gaussian, data))
+# data_gaussian_derivative = list(map(af.FullyDiff.gaussian_derivative, data))
+# plt.plot(data, data_gaussian, label='Gaussiana')
+# plt.plot(data, data_gaussian_derivative, label='Derivada Gaussiana')
+
+# data_linear = list(map(af.FullyDiff.linear, data))
+# data_linear_derivative = list(map(af.FullyDiff.linear_derivative, data))
+# plt.plot(data, data_linear, label='Linear')
+# plt.plot(data, data_linear_derivative, label='Derivada Linear')
+
+# data_exponential_linear_unit = list(map(af.FullyDiff.exponential_linear_unit, data))
+# data_exponential_linear_unit_derivative = list(map(af.FullyDiff.exponential_linear_unit_derivative, data))
+# plt.plot(data, data_exponential_linear_unit, label='ELU')
+# plt.plot(data, data_exponential_linear_unit_derivative, label='Derivada ELU')
+
+plt.legend()
+plt.show()
