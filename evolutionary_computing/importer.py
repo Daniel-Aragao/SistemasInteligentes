4da5274996d9b4bf5c11515dd62f797a81fd3187
Daniel Aragão <danielfilhoce@gmail.com>
2019-06-04 12:23:14 -0300

    importer and euclidian calculation

---
+++ evolutionary_computing/importer.py
@@ -0,0 +1,20 @@
+class Importer:
+    
+    def import_cities(path):
+        with open(path) as file_obj:
+            cities = []
+            cities_size = 0
+            
+            for line in file_obj:
+                clean_line = line.strip()
+                
+                if clean_line:
+                    splited_line = clean_line.split()
+                    
+                    if(not cities_size):
+                        cities_size = int(splited_line[0])
+                    else:
+                        cities.append((splited_line[0], splited_line[1]))
+                        
+            
+        return cities_size, cities
---
+++ evolutionary_computing/util.py
@@ -0,0 +1,6 @@
+import math
+
+
+class Util:
+    def euclidian_distance(x1, y1, x2, y2):
+        return math.sqrt((x1-x2) ** 2 + (y1-y2) ** 2)
